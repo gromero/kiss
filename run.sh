fd85727bcdefcead81b79b83d1df5ceb3b3ee388
Gustavo Romero <gustavo.romero@linaro.org>
2022-10-05 00:15:24 +0000

    Adjust run.sh now we load from file passed in cli

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ gcc -c hann_window.c -o hann_window.o # Window
 gcc -c load_raw.c -o load_raw.o # Load Raw
 gcc -c  main.c -o main.o -I./include -L./lib -lkissfft-float # Main
 gcc main.o hann_window.o load_raw.o -o main -I./include -L./lib -lkissfft-float -lm # ALL
-LD_LIBRARY_PATH=./lib ./main # Run
+LD_LIBRARY_PATH=./lib ./main ./yes_from_board_v2.raw # Run
 
