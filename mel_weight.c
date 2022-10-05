da22cdb5dd63154a5ba603bcdc547ba60f458d76
Gustavo Romero <gustavo.romero@linaro.org>
2022-10-05 23:20:07 +0000

    Initial stab on mel_weight dot product

---
+++ mel_weight.c
@@ -0,0 +1,8 @@
+#include "mel_weight.h"
+
+// dot product [1, 257] x [257, 40] (mel weight)
+// That's pretty much a dot product of matrices.
+void apply_mel_weight(float *s /* spectrum bins = 257 */)
+{
+    // to be implemented.
+}
